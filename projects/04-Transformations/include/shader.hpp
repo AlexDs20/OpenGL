5ed2b2d575e8e5f0121e77b1c72e36f76cf8ac58
Alexandre De Spiegeleer <alex-ds@live.be>
2023-03-11 18:06:44 +0100

    Moved shader code to cpp file, kept definition in hpp

--- projects/04-Transformations/include/shader.hpp
+++ projects/04-Transformations/include/shader.hpp
@@ -1,13 +1,8 @@
-
 #ifndef SHADER_H
 #define SHADER_H
-
 #include <glad/glad.h>
 
 #include <string>
-#include <fstream>
-#include <sstream>
-#include <iostream>
 #include <glm/glm.hpp>
 
 
@@ -18,115 +13,15 @@ class Shader
     unsigned int ID;
 
     // constructor reads and builds the shader
-    Shader(const char* vertexPath, const char* fragmentPath)
-    {
-      // get source code from file
-      std::string vertexCode;
-      std::string fragmentCode;
-      std::ifstream vShaderFile;
-      std::ifstream fShaderFile;
+    Shader(const char* vertexPath, const char* fragmentPath);
 
-      // ensure ifstream can throw exceptions
-      vShaderFile.exceptions( std::ifstream::failbit | std::ifstream::badbit);
-      fShaderFile.exceptions( std::ifstream::failbit | std::ifstream::badbit);
-
-      try
-      {
-        // open files
-        vShaderFile.open(vertexPath);
-        fShaderFile.open(fragmentPath);
-
-        // read file's buffer contents into streams
-        std::stringstream vShaderStream, fShaderStream;
-        vShaderStream << vShaderFile.rdbuf();
-        fShaderStream << fShaderFile.rdbuf();
-
-        // close file handlers
-        vShaderFile.close();
-        fShaderFile.close();
-
-        // convert stream into string
-        vertexCode = vShaderStream.str();
-        fragmentCode = fShaderStream.str();
-      }
-      catch(std::ifstream::failure e)
-      {
-        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-      }
-      const char* vShaderCode = vertexCode.c_str();
-      const char* fShaderCode = fragmentCode.c_str();
-
-      // compile
-      unsigned int vertex, fragment;
-      int success;
-      char infoLog[512];
-
-      // compile vertex
-      vertex = glCreateShader(GL_VERTEX_SHADER);
-      glShaderSource(vertex, 1, &vShaderCode, NULL);
-      glCompileShader(vertex);
-      // error printing
-      glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
-      if (!success)
-      {
-        glGetShaderInfoLog(vertex, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-      }
-
-      // compile fragment
-      fragment = glCreateShader(GL_FRAGMENT_SHADER);
-      glShaderSource(fragment, 1, &fShaderCode, NULL);
-      glCompileShader(fragment);
-      // error printing
-      glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
-      if (!success)
-      {
-        glGetShaderInfoLog(fragment, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-      }
-
-      // shader program
-      ID = glCreateProgram();
-      glAttachShader(ID, vertex);
-      glAttachShader(ID, fragment);
-      glLinkProgram(ID);
-
-      // check linking errors
-      glGetProgramiv(ID, GL_LINK_STATUS, &success);
-      if (!success)
-      {
-        glGetProgramInfoLog(ID, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-      }
-
-      glDeleteShader(vertex);
-      glDeleteShader(fragment);
-
-    }
-
-    void use()     // Active shader programe
-    {
-      glUseProgram(ID);
-    }
+    // Use this shade program
+    void use();
 
     // set uniform values
-    void setBool(const std::string &name, bool value) const
-    {
-      glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
-    }
-    void setInt(const std::string &name, int value) const
-    {
-      glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
-    }
-    void setFloat(const std::string &name, float value) const
-    {
-      glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
-    }
-    void setMat4f(const std::string &name, const glm::mat4 &mat) const
-    {
-      glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
-    }
-
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+    void setMat4f(const std::string &name, const glm::mat4 &mat) const;
 };
-
 #endif
