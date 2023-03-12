aa41daeb35cd3d5ad50a2ee91a08971ba64ccc67
Alexandre De Spiegeleer <alex-ds@live.be>
2023-03-12 21:48:18 +0100

    Use predefined 'camera'

--- projects/06-Camera/src/main.cpp
+++ projects/06-Camera/src/main.cpp
@@ -202,9 +202,19 @@ int main(int argc, char** argv)
     ourShader.use();
     ourShader.setInt("texture1", 0);
 
-    glm::mat4 view = glm::mat4(1.0f);
+    // Camera
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 5.0f);
+
+    // coordinates
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    //glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    //glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+    //glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+    glm::mat4 view = glm::lookAt(cameraPos, cameraTarget, up);
+
     glm::mat4 proj;
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -10.0f));
     proj = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
     glEnable(GL_DEPTH_TEST);
@@ -224,18 +234,25 @@ int main(int argc, char** argv)
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
 
+        float time = glfwGetTime();
+
         // Select shader program, bind VAO and draw!
         ourShader.use();
-        ourShader.setMat4f("view", view);
         ourShader.setMat4f("proj", proj);
 
+        const float radius = 10.0f;
+        float camX = sin(time) * radius;
+        float camZ = cos(time) * radius;
+        view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), cameraTarget, up);
+        ourShader.setMat4f("view", view);
+
         glBindVertexArray(VAO);
+        // Go through each box to render
         for (unsigned int i=0; i<10; ++i)
         {
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i+1;
-            float time = glfwGetTime();
             model = glm::rotate(model, glm::radians(angle*time), glm::normalize(glm::vec3(0.4f, 1.0f, 0.3f)));
             ourShader.setMat4f("model", model);
             glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
