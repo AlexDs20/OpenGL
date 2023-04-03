22f5317e68c471bd012f928aaad32a534f2d1ac2
Alexandre De Spiegeleer <alex-ds@live.be>
2023-04-03 22:52:13 +0200

    change bg color

--- projects/10-Colors/src/main.cpp
+++ projects/10-Colors/src/main.cpp
@@ -198,7 +198,7 @@ int main(int argc, char** argv)
         processInput(window);
 
         // rendering commands
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Set camera transforms
