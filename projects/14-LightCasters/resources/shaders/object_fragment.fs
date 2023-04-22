ae498e03377514183946331119385b6cb1c689d3
Alexandre De Spiegeleer <alex-ds@live.be>
2023-04-22 20:50:38 +0200

    Light caster Directional light

---
+++ projects/14-LightCasters/resources/shaders/object_fragment.fs
@@ -0,0 +1,48 @@
+#version 330 core
+struct Light {
+   vec3 direction;
+
+   vec3 position;
+   vec3 ambient;
+   vec3 diffuse;
+   vec3 specular;
+};
+
+struct Material {
+   sampler2D diffuse;
+   sampler2D specular;
+   float shininess;
+};
+
+uniform Light light;
+uniform Material material;
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoord;
+
+// viewPos is here needed only because we compute the lighting in world space and not viewspace
+// We can actually compute in view space and not need viewPos here because viewPos is always (0,0,0) in that case.
+uniform vec3 viewPos;
+
+out vec4 FragColor;
+
+void main()
+{
+   // Ambient light
+   vec3 ambient = light.ambient * texture(material.diffuse, TexCoord).rgb;
+
+   // Diffuse light
+   vec3 norm = normalize(Normal);
+   vec3 lightDir = normalize(-light.direction); // normalize(light.position - FragPos);
+   float diff = max(dot(norm, lightDir), 0.0);
+   vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoord).rgb;
+
+   // Specular light
+   vec3 viewDir = normalize(viewPos - FragPos);
+   vec3 reflectDir = reflect(-lightDir, norm);
+   float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+   vec3 specular = light.specular * spec * texture(material.specular, TexCoord).rgb;
+
+   FragColor = vec4(ambient + diffuse + specular, 1.0);
+}
